6ae6bf98704a764b5631ccbdee26ea9799e8faeb
Helmuth Breitenfellner <helmuth@breitenfellner.at>
2019-04-30 07:35:53 +0200

    Empty framework

---
+++ ProgrammingExercise/src/kMST_ILP.cpp
@@ -0,0 +1,83 @@
+#include "kMST_ILP.h"
+
+kMST_ILP::kMST_ILP( Instance& _instance, string _model_type, int _k ) :
+	instance( _instance ), model_type( _model_type ), k( _k ), epInt( 0.0 ), epOpt( 0.0 )
+{
+	n = instance.n_nodes;
+	m = instance.n_edges;
+	if( k == 0 ) k = n;
+}
+
+void kMST_ILP::solve()
+{
+	// initialize CPLEX solver
+	initCPLEX();
+
+	// ++++++++++++++++++++++++++++++++++++++++++
+	// TODO create variables and build constraints
+	// ++++++++++++++++++++++++++++++++++++++++++
+
+	try {
+		// build model
+		cplex = IloCplex( model );
+		cplex.exportModel( "model.lp" );
+
+		// set parameters
+		epInt = cplex.getParam( IloCplex::EpInt );
+		epOpt = cplex.getParam( IloCplex::EpOpt );
+		// only use a single thread
+		cplex.setParam( IloCplex::Threads, 1 );
+
+		// set cut- and lazy-constraint-callback for
+		// cycle-elimination cuts ("cec") or directed connection cuts ("dcc")
+		CutCallback* usercb = new CutCallback( env, model_type, epOpt, instance, x, z );
+		CutCallback* lazycb = new CutCallback( env, model_type, epOpt, instance, x, z );
+		cplex.use( (UserCutI*) usercb );
+		cplex.use( (LazyConsI*) lazycb );
+
+		// solve model
+		cout << "Calling CPLEX solve ...\n";
+		cplex.solve();
+		cout << "CPLEX finished.\n\n";
+		cout << "CPLEX status: " << cplex.getStatus() << "\n";
+		cout << "Branch-and-Bound nodes: " << cplex.getNnodes() << "\n";
+		cout << "Objective value: " << cplex.getObjValue() << "\n";
+		cout << "CPU time: " << Tools::CPUtime() << "\n\n";
+
+	}
+	catch( IloException& e ) {
+		cerr << "kMST_ILP: exception " << e.getMessage();
+		exit( -1 );
+	}
+	catch( ... ) {
+		cerr << "kMST_ILP: unknown exception.\n";
+		exit( -1 );
+	}
+}
+
+// ----- private methods -----------------------------------------------
+
+void kMST_ILP::initCPLEX()
+{
+	cout << "initialize CPLEX ... ";
+	try {
+		env = IloEnv();
+		model = IloModel( env );
+		values = IloNumArray( env );
+	}
+	catch( IloException& e ) {
+		cerr << "kMST_ILP: exception " << e.getMessage();
+	}
+	catch( ... ) {
+		cerr << "kMST_ILP: unknown exception.\n";
+	}
+	cout << "done.\n";
+}
+
+kMST_ILP::~kMST_ILP()
+{
+	// free CPLEX resources
+	cplex.end();
+	model.end();
+	env.end();
+}
---
+++ ProgrammingExercise/src/kMST_ILP.h
@@ -0,0 +1,44 @@
+#ifndef __KMST_ILP__H__
+#define __KMST_ILP__H__
+
+#include "Tools.h"
+#include "Instance.h"
+#include "CutCallback.h"
+#include <ilcplex/ilocplex.h>
+
+using namespace std;
+
+ILOSTLBEGIN
+
+class kMST_ILP
+{
+
+private:
+
+	Instance& instance;
+	string model_type;
+	int k;
+	u_int m, n;
+
+	IloEnv env;
+	IloModel model;
+	IloCplex cplex;
+
+	IloBoolVarArray x; // edge or arc selection variables
+	IloBoolVarArray z; // node selection variables
+
+	IloNumArray values; // to store result values of x
+
+	double epInt, epOpt;
+
+	void initCPLEX();
+
+public:
+
+	kMST_ILP( Instance& _instance, string _model_type, int _k );
+	~kMST_ILP();
+	void solve();
+
+};
+
+#endif //__KMST_ILP__H__
