68e4516a2d5045638200872245f5000d02c89408
Helmuth Breitenfellner <helmuth@breitenfellner.at>
2019-05-03 17:35:33 +0200

    Formatting issue

--- ProgrammingExercise/src/kMST_SCF.h
+++ ProgrammingExercise/src/kMST_SCF.h
@@ -7,13 +7,13 @@ class kMST_SCF : public kMST_ILP {
 
 protected:
   // SCF variables
-	IloNumVarArray f; // flow variables
+  IloNumVarArray f; // flow variables
 
   void createModel();
   void outputVars();
 
 public:
-  	kMST_SCF( Digraph& _digraph, int _k );
+  kMST_SCF( Digraph& _digraph, int _k );
 
 };
 
